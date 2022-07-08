63ae4404692be25bccc83f8fa163b069b6435b63
Dongua <597449675@qq..com>
2022-07-08 11:21:55 +0800

    删除部分文章

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -38,8 +38,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                     {
                         text: "学习笔记",
                         items: [
-                            { text: "《JavaScript教程》", link: "/note/javascript/" },
-                            { text: "《JavaScript高级程序设计》", link: "/note/js/" },
+                            // { text: "《JavaScript教程》", link: "/note/javascript/" },
+                            // { text: "《JavaScript高级程序设计》", link: "/note/js/" },
                             { text: "《ES6 教程》", link: "/note/es6/" },
                             { text: "《Vue》", link: "/note/vue/" },
                             { text: "《React》", link: "/note/react/" },
@@ -362,25 +362,25 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 },
             },
         ],
-        [
-            "vuepress-plugin-comment", // 评论
-            {
-                choosen: "gitalk",
-                options: {
-                    clientID: "a6e1355287947096b88b",
-                    clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
-                    repo: "blog-gitalk-comment", // GitHub 仓库
-                    owner: "xugaoyi", // GitHub仓库所有者
-                    admin: ["xugaoyi"], // 对仓库有写权限的人
-                    // distractionFreeMode: true,
-                    pagerDirection: "last", // 'first'正序 | 'last'倒序
-                    id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
-                    title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
-                    labels: ["Gitalk", "Comment"], // GitHub issue 的标签
-                    body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
-                },
-            },
-        ],
+        // [
+        //     "vuepress-plugin-comment", // 评论
+        //     {
+        //         choosen: "gitalk",
+        //         options: {
+        //             clientID: "a6e1355287947096b88b",
+        //             clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
+        //             repo: "blog-gitalk-comment", // GitHub 仓库
+        //             owner: "xugaoyi", // GitHub仓库所有者
+        //             admin: ["xugaoyi"], // 对仓库有写权限的人
+        //             // distractionFreeMode: true,
+        //             pagerDirection: "last", // 'first'正序 | 'last'倒序
+        //             id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+        //             title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+        //             labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+        //             body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
+        //         },
+        //     },
+        // ],
         [
             "@vuepress/last-updated", // "上次更新"时间格式
             {
